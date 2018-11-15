cc201390d33cfd9e3862e44b9a91a415c77a0c8d
DESKTOP-BC3V8EV\Tobias <tobis-stroedicke@gmx.de>
2018-11-15 22:52:35 +0100

    New House

---
+++ The_Walking_Dead/Assets/GetDoorCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetDoorCube : MonoBehaviour {
+    /// <summary>The Transform of the Door(Cube)</summary>
+    public static Transform m_DoorCube;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        // Get Doof
+        Transform[] temp = GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < temp.GetLength(0); i++)
+        {
+            if(temp[i].name == "DoorCube")
+            {
+                // set Door to static Transform
+                m_DoorCube = temp[i];
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
